a65e1289c781f88ead0ee69cedb9e7e12a538766
Oliver_asm <oliverantoniosotomeran@gmail.com>
2025-10-02 01:42:46 -0400

    Create the function that genarate the message in the console

--- One_Piece_Characters.js
+++ One_Piece_Characters.js
@@ -2,12 +2,23 @@
 const categories = ["Pirate", "Marine", "Bounty Hunter","Holy Knight"];
 const devilfruits = ["Sand", "Sun God","Dragon","Magma","Invisible Man", "Earthquake"];
 const races = ["Tontatta","Mink", "Buccaneer", "Lunarian", "Giant", "Fish-Man", "Human"];
-const intention = ["Money","Power", "Fame", "End Corruption","the Greatest Swordsman","the One Piece","Revenge"]
+const intentions = ["Money","Power", "Fame", "End Corruption"," being the Greatest Swordsman","the One Piece","Revenge"]
 
 
-function selectRandom(margenArray) {
+function selectRandomWord(margenArray) {
     return Math.floor(Math.random() * margenArray.length);
 }
 
+function createMessage(){
+    let race = races[selectRandomWord(races)];
+    let category = categories[selectRandomWord(categories)];
+    let devilfruit = devilfruits[selectRandomWord(devilfruits)];
+    let intention = intentions[selectRandomWord(intentions)];
 
+    let message = `You are a ${race} ${category} with the ${devilfruit} devil fruit, in search of ${intention}.`
 
+    console.log(message);
+}
+
+console.log("Your character from the One Piece world is: \n")
+createMessage();
