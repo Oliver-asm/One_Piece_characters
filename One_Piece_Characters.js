25c8a998303b77b05eb93aaffd6c56da7aa220d6
Oliver_asm <oliverantoniosotomeran@gmail.com>
2025-10-02 01:27:41 -0400

    Create arrays which content several words for our random message

--- One_Piece_Characters.js
+++ One_Piece_Characters.js
@@ -1,10 +1,13 @@
 
+const categories = ["Pirate", "Marine", "Bounty Hunter","Holy Knight"];
+const devilfruits = ["Sand", "Sun God","Dragon","Magma","Invisible Man", "Earthquake"];
+const races = ["Tontatta","Mink", "Buccaneer", "Lunarian", "Giant", "Fish-Man", "Human"];
+const intention = ["Money","Power", "Fame", "End Corruption","the Greatest Swordsman","the One Piece","Revenge"]
 
 
-/*function selectRandom(margenArray) {
+function selectRandom(margenArray) {
     return Math.floor(Math.random() * margenArray.length);
 }
 
-*/
 
-console.log("Hello world");
+
